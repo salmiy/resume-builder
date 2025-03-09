c45aa65bb37f18a99f608dead7f596ed824e3d3d
Salmi Youssef <salmi19971@gmail.com>
2025-03-09 15:03:31 +0000

    improving wording

--- components/Template/defaults.ts
+++ components/Template/defaults.ts
@@ -2,7 +2,16 @@
 
 export const defaultJsCode = `
 /**
- * MUST INCLUDE a Template function
+ * The code you write here will only affect the version you have.
+ * The code you write here must be valid react code.
+ * 
+ * If you wish to share a template send it as attachment to salmi19971@gmail.com.
+ */
+
+/**
+ * 
+ * The Code Have to have a function called 'Template' that take an argument
+ * of type DataScheme (DataScheme is defined in the end of the file)
  */
 
 function Template({ data }) {
@@ -21,12 +30,115 @@ function Template({ data }) {
             </div>
         </div>
     )
-}`
+}
+/*
+type DataScheme = {
+    basics: {
+        image: string,
+        name: string,
+        email: string,
+        phone: string,
+        address: string,
+        title: string,
+        summary: {
+            enabled: boolean,
+            data: string,
+        }
+    },
+    experience: {
+        enabled: boolean,
+        data: {
+            id?: number,
+            enabled?: boolean,
+            position: string,
+            company: string,
+            location: string,
+            startDate: string,
+            endDate: string,
+            description: string,
+            bulletPoints: string[],
+            technologies: {
+                icon: string,
+                name: string
+            }[]
+        }[]
+    },
+    education: {
+        enabled: boolean,
+        data: {
+            id?: number,
+            enabled?: boolean,
+            degree: string,
+            university: string,
+            location: string,
+            startDate: string,
+            endDate: string,
+            bulletPoints: string[]
+        }[]
+    },
+    projects: {
+        enabled: boolean,
+        data: {
+            id?: number,
+            enabled?: boolean,
+            name: string,
+            url?: string,
+            startDate: string,
+            endDate: string,
+            description: string,
+            bulletPoints: string[],
+            technologies: {
+                icon: string,
+                name: string
+            }[]
+        }[]
+    },
+    links: {
+        enabled: boolean,
+        data: {
+            enabled?: boolean,
+            icon: string,
+            name: string,
+            userId: string,
+            url: string
+        }[]
+    },
+    skills: {
+        enabled: boolean,
+        data: {
+            enabled?: boolean,
+            icon?: string,
+            name: string,
+            category: string,
+            rating: number
+        }[]
+    },
+    languages: {
+        enabled: boolean,
+        data: {
+            enabled?: boolean,
+            name: string,
+            rating: number
+        }[]
+    },
+    hobbies: {
+        enabled: boolean,
+        data: {
+            enabled?: boolean,
+            name: string
+        }[]
+    }
+}
+*/
+`
 
 export const defaultCssCode = `
 /*
+ * You can write your css styling here.
+ *
+ * If you use Tailwind you can use Play CDN:
+ * @import url('https://unpkg.com/@tailwindcss/browser@4')
+ * https://tailwindcss.com/docs/installation/play-cdn 
  * 
- * you can only include @font-face @import
- * 
-**/
+ */
 `
